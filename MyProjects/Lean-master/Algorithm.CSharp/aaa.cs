ed2883378682ac008836118c027b01930bbe6246
3123958139 <3123958139@qq.com>
2024-04-26 17:37:17 +0800

    添加一些运行成功的效果图

--- MyProjects/Lean-master/Algorithm.CSharp/aaa.cs
+++ MyProjects/Lean-master/Algorithm.CSharp/aaa.cs
@@ -19,12 +19,16 @@ namespace QuantConnect.Algorithm.CSharp
 
         public override void Initialize()
         {
+            //本地已经生成stk000000~stk000999的1998~2024的日线数据
             SetStartDate(2023, 10, 07);
             SetEndDate(2023, 11, 11);
             SetCash(100000);
+            //订阅数据
             symbol = AddEquity("stk000001", Resolution.Daily).Symbol;
             AddEquity("stk000000", Resolution.Daily);
+            //设置基础资产，用来算一些指标用到
             SetBenchmark("stk000000");
+            //图表初始化
             var avgCross = new Chart("Strategy Equity");
             var fastMa = new Series("FastMA", SeriesType.Line, 1);
             var slowMa = new Series("SlowMA", SeriesType.Line, 1);
@@ -36,20 +40,20 @@ namespace QuantConnect.Algorithm.CSharp
 
         public override void OnData(Slice slice)
         {
+            //测试Debug的通信
             Debug(slice.Bars["stk000001"].Open.ToString("F2"));
+            //测试Order的通信
             if (!Portfolio.Invested)
             {
                 SetHoldings(symbol, 1);
                 Debug("Purchased Stock");
             }
+            //测试Chart的通信
             _lastPrice = slice["stk000001"].Close;
-
             if (_fastMa == 0) _fastMa = _lastPrice;
             if (_slowMa == 0) _slowMa = _lastPrice;
-
             _fastMa = (0.01m * _lastPrice) + (0.99m * _fastMa);
             _slowMa = (0.001m * _lastPrice) + (0.999m * _slowMa);
-
             if (Time > _resample)
             {
                 _resample = Time.Add(_resamplePeriod);
